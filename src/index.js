9a1146531954c1f4a2ebaf988a680e9ee0d928f0
Daniel Caldas <daniel.caldas@trivago.com>
2019-02-11 16:52:12 +0100

    Small review on index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+/**
+ * @module babel-plugin-cloudinary
+ * @description
+ * Plugin entry point makes use of the auxiliary modules
+ * to compile URLs. The core flow of the plugin is inside
+ * the processUrl method.
+ */
 const t = require("babel-types");
 const _get = require("lodash/get");
 const astHelpers = require("./ast-helpers");
@@ -5,9 +12,9 @@ const { PLUGIN_PARAMETERS } = require("./plugin-parameters");
 const { CALLEE_NAME, getBaseImageUrl } = require("./cloudinary-proxy");
 
 /**
- * Function that given a path that contains a call expression for
- * building a cloudinary URL performs the traverse and compiles the call expression
- * into the actual URL.
+ * Given a path that contains a call expression for
+ * building a cloudinary URL, this functions performs the traverse
+ * and compiles the call expression into the actual URL.
  * @param {Object} path - the path to traverse.
  * @returns {undefined}
  */
@@ -19,19 +26,16 @@ function processUrl(path) {
     PLUGIN_PARAMETERS.transforms.placeholder
   );
 
-  // FIXME: avoid deletion or make it more clear
+  // FIXME: avoid deletion or make it more clear?
   delete parameters.transforms;
 
-  // URL generation
   const url = getBaseImageUrl(PLUGIN_PARAMETERS.assetName.placeholder, staticBaseTransforms);
   const { quasis: baseQuasis, expressions: baseExpressions } = astHelpers.convertUrlIntoTemplateLiteral(
     url,
     mappings,
     PLUGIN_PARAMETERS.transforms.placeholder
   );
-  // TODO: after doing the above we need to sort the quasis and expressions by ORD?? Still not clear this implementation
   const allPluginParameters = Object.assign({}, parameters, { assetName });
-  // TODO: move to method ??buildComplementaryExpressions (naming!!)
   const extraExpressions = Object.keys(PLUGIN_PARAMETERS)
     .sort((paramA, paramB) => PLUGIN_PARAMETERS[paramA].ord > PLUGIN_PARAMETERS[paramB].ord)
     .map(param => {
@@ -43,21 +47,13 @@ function processUrl(path) {
         return babelConstructor(PLUGIN_PARAMETERS[param].default);
       }
     });
-
   const expressions = [...baseExpressions, ...extraExpressions.filter(Boolean)];
-  // TODO: move to method buildPlaceholderQuasisForExpressions
-  // ...${isMobile ? extMobile : extDesktop}${}`;
-  // ...${isMobile ? extMobile : extDesktop}`;"
-  // FIXME: Find a way to remove overhead on the empty placeholders ${}
+  // FIXME: Find a way to remove overhead on the empty placeholders ${} e.g. "test/fixtures/only-transforms-provided"
   const quasis = [...baseQuasis, ...Object.keys(extraExpressions).map(() => astHelpers.templateElement(""))];
 
   path.replaceWith(t.expressionStatement(t.templateLiteral(quasis, expressions)));
 }
 
-/**
- * Plugin entry point.
- * @returns {undefined}
- */
 module.exports = function babelPluginCloudinary() {
   return {
     visitor: {
