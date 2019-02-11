84187385506e9f357921789457baa754e0515ce2
Daniel Caldas <daniel.caldas@trivago.com>
2019-02-11 16:19:29 +0100

    Clean plugin-parameters

--- src/plugin-parameters.js
+++ src/plugin-parameters.js
@@ -1,14 +1,21 @@
-// TODO: <host>
+/**
+ * @module plugin-parameters
+ * @description
+ * This configuration file holds all the metadata associated with the
+ * parameters that are part of babel-plugin-cloudinary API. As as
+ * overview below is the composition of an URL and the position where
+ * each parameter is allocated:
+ *
+ * http[s]://host/<transforms>/<prefix><assetName><postfix><resourceExtension>
+ */
+// TODO: support <host> making it replaceable at runtime
 // FIXME: cleanup properties that are not being used
-// http[s]://<host>/<transforms>/<prefix><assetName><postfix><resourceExtension>
-// TODO: run unit tests assertions on this PLUGIN_PARAMETERS (key and ord are mandatory, the others
-// must be protected and kept untouched, defaultType must be in babel-types)
 const PLUGIN_PARAMETERS = {
   transforms: {
     key: "transforms",
     ord: 1,
     placeholder: "TRANSFORM",
-    validate: () => true, // TODO: run validate methods for all expressions
+    validate: () => true, // TODO: run validate methods for all parameters
   },
   postfix: {
     key: "postfix",
