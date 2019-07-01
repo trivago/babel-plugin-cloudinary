b08a7add1e4e931dd20fab5181a756f1587f0f99
Daniel Caldas <daniel.caldas@trivago.com>
2019-07-01 16:03:17 +0200

    Fix lint issues. Add prettier

--- test/cloudinary-proxy.spec.js
+++ test/cloudinary-proxy.spec.js
@@ -4,6 +4,7 @@ jest.mock(
   "../cloudinaryrc.json",
   () => ({
     native: {
+      // eslint-disable-next-line camelcase
       cloud_name: "trivago",
       secure: true,
     },
@@ -16,11 +17,13 @@ jest.mock(
 describe("cloudinary-proxy", () => {
   it("has proper output for getBaseImageUrl 1", () => {
     const expected = "https://trivago.images.com/";
+
     expect(getBaseImageUrl("myfile.jpg")).toEqual(expected);
   });
 
   it("has proper output for getBaseImageUrl 2", () => {
     const expected = "https://trivago.images.com/";
+
     expect(getBaseImageUrl("myfile_2@.jpg")).toEqual(expected);
   });
 });
